f3f270b2832252acd518b54c472c9a143304720a
Sam Page <samnpage96@gmail.com>
2024-04-29 16:56:19 -0400

    Initialized getMedian function

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
+const getMedian = (array) => {
+    const sorted = array.sort((a, b) => {
+        return a - b
+    });
+}
+
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
 
     const mean = getMean(numbers);
-    
+
     document.querySelector("#mean").textContent = mean;
 }
